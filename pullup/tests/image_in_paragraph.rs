1fc3f6ffc934511e984a35203bba509bdbbdae86
Jingdong Xu <jingdong.xu@shopee.com>
2025-11-21 00:13:24 +0800

    Revert "support pic"
    This reverts commit 7635f319c516fb0fc9f8bfe45ea04a4ae429f6d8.

--- pullup/tests/image_in_paragraph.rs
+++ pullup/tests/image_in_paragraph.rs
@@ -4,17 +4,15 @@ fn test_image_after_text_in_same_paragraph() {
     use pulldown_cmark::Parser;
     use pullup::markdown::MarkdownIter;
     use pullup::markdown::to::typst::{ConvertImages, ConvertParagraphs, ConvertText};
-    use pulldown_typst::markup::TypstMarkup;
     
     let md = "\
 整体交互流程图
-
-![整体交互流程图](./images/infeed/image2.png)
+![整体交互流程图](./images/infeed/image2.png)  
 ";
     
     // Convert markdown to typst events
-    let events = ConvertImages::new(
-        ConvertText::new(
+    let events = ConvertText::new(
+        ConvertImages::new(
             ConvertParagraphs::new(
                 MarkdownIter(Parser::new(&md))
             )
@@ -52,34 +50,5 @@ fn test_image_after_text_in_same_paragraph() {
     assert!(found_text, "Should find text");
     assert!(found_paragraph_end_before_image, "Paragraph should be closed before image");
     assert!(found_image, "Should find image function call");
-    
-    // Also verify the final output
-    let events2 = ConvertImages::new(
-        ConvertText::new(
-            ConvertParagraphs::new(
-                MarkdownIter(Parser::new(&md))
-            )
-        )
-    );
-    
-    let typst_events: Vec<_> = events2
-        .filter_map(|e| {
-            match e {
-                pullup::ParserEvent::Typst(te) => Some(te),
-                _ => None,
-            }
-        })
-        .collect();
-    
-    let output: String = TypstMarkup::new(typst_events.into_iter()).collect();
-    
-    println!("Final output:\n{}", output);
-    
-    // Expected output: paragraph with text, then image on separate line
-    assert!(output.contains("#par()[整体交互流程图]"), "Should have paragraph with text");
-    assert!(output.contains("#image(\"images/infeed/image2.png\")"), "Should have image");
-    
-    // Should NOT have linebreak before image in the paragraph
-    assert!(!output.contains("#par()[整体交互流程图#linebreak()"), "Should not have linebreak before closing paragraph");
 }
 
